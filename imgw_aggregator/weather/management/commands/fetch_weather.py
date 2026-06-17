38fccc51355ffa41d4201bb6b4de8722003fc4c9
PiotrFrasik <frasik.piotrek@gmail.com>
2026-06-17 19:17:58 +0200

    Handle fetch_weather errors.

--- imgw_aggregator/weather/management/commands/fetch_weather.py
+++ imgw_aggregator/weather/management/commands/fetch_weather.py
@@ -9,13 +9,18 @@ class Command(BaseCommand):
 
     def create_station_object(self, station_data):
         """Create a new station object"""
-        return Station.objects.create(name=station_data["nazwa_stacji"],
+        try:
+            return Station.objects.create(name=station_data["nazwa_stacji"],
                                  code=station_data["kod_stacji"],
                                  latitude=station_data["lat"],
                                  longitude=station_data["lon"])
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error creating station: {e}"))
+        return None
 
     def create_weather_measurement_object(self, station_data):
         """Create a new weather measurement object"""
+        
         def datatime_converting(datatime_str):
             """Converting str to datetime"""
             if datatime_str is None:
@@ -28,24 +33,38 @@ class Command(BaseCommand):
             """Converting str to float"""
             if data is None:
                 return None
-            return float(data)
 
-        station = Station.objects.get(code=station_data["kod_stacji"])
+            try:
+                data = float(data)
+            except ValueError:
+                self.stdout.write("Value error")
+                data = None
+            return data
 
-        return WeatherMeasurement.objects.create(station=station,
-                                                ground_temp = float_converting(station_data["temperatura_gruntu"]),
-                                                ground_temp_date = datatime_converting(station_data["temperatura_gruntu_data"]),
-                                                air_temp =  float_converting(station_data["temperatura_powietrza"]),
-                                                air_temp_date = datatime_converting(station_data["temperatura_powietrza_data"]),
-                                                wind_speed =  float_converting(station_data["wiatr_srednia_predkosc"]),
-                                                humidity = float_converting(station_data["wilgotnosc_wzgledna"]),
-                                                rainfall = float_converting(station_data["opad_10min"]))
-
+        try:
+            station = Station.objects.get(code=station_data["kod_stacji"])
+            return WeatherMeasurement.objects.create(station=station,
+                                              ground_temp=float_converting(station_data["temperatura_gruntu"]),
+                                              ground_temp_date=datatime_converting(
+                                                  station_data["temperatura_gruntu_data"]),
+                                              air_temp=float_converting(station_data["temperatura_powietrza"]),
+                                              air_temp_date=datatime_converting(
+                                                  station_data["temperatura_powietrza_data"]),
+                                              wind_speed=float_converting(station_data["wiatr_srednia_predkosc"]),
+                                              humidity=float_converting(station_data["wilgotnosc_wzgledna"]),
+                                              rainfall=float_converting(station_data["opad_10min"]))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error getting object - station : {e}"))
+        return None
 
     def handle(self, *args, **kwargs):
         # Download weather's data from IMGW
         link = "https://danepubliczne.imgw.pl/api/data/meteo/"
-        data = requests.get(link)
+        try:
+            data = requests.get(link, timeout=10)
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error downloading data: {e}"))
+            return
 
         if data.status_code == 200:
             stations_data = data.json()
@@ -58,13 +77,5 @@ class Command(BaseCommand):
                         self.create_station_object(station_info)
                         # Update the set of existing station codes
                         existing_station_codes.add(station_info['kod_stacji'])
-
                     # Create weather measurement object
                     self.create_weather_measurement_object(station_info)
-
-
-
-
-
-
-
